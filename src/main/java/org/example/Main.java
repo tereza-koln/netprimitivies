496c54aedb78fcf42fad27e6571f570e5f5e5c46
Tereza <ruanapoor@gmail.com>
2023-10-07 07:39:05 +0300

    first

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int price = 16_676;
+
+        int miles = price / 20;
+        System.out.println("Бонусных миль:" + miles);
+    }
+}
